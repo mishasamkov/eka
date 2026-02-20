3e7ad79503a642e4c8721e852461621ac347b5ff
ANosok <kirill60278@gmail.com>
2026-02-20 11:19:41 +0500

    w

---
+++ Command.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Command
+    {
+        public string Message { get; set; }
+    }
+}
