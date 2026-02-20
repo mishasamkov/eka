876f172c246ec682f231c53f1261a2d34ba4a694
ANosok <kirill60278@gmail.com>
2026-02-20 10:25:47 +0500

    й

---
+++ News.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+//https://pspu.ru/about_the_university/news/
+namespace Common
+{
+    public class News
+    {
+        public string Img { get; set; }
+        public DateTime Date { get; set; }
+        public string Badge { get; set; }
+        public string Title { get; set; }
+        public string Src { get; set; }
+
+        public News(string img,DateTime date, string badge,string title,string src) 
+        
+        
+        { 
+            this.Img = img;
+            this.Date = date;
+            this.Badge = badge;
+            this.Title = title;
+            this.Src = src;
+        }
+    }   
+}
